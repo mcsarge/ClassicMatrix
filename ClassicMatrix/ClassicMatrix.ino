c737e7fa491b5f3eefa33e66bcb030d3f8d96067
Matt Sargent <matthew.c.sargent@gmail.com>
2020-04-24 11:44:31 -0400

    update from FeatureDemo

--- ClassicMatrix/ClassicMatrix.ino
+++ ClassicMatrix/ClassicMatrix.ino
@@ -3,23 +3,17 @@
     This example code is released into the public domain
 */
 
-//  #include <SmartLEDShieldV4.h>  // comment out this line for if you're not using SmartLED Shield V4 hardware (this line needs to be before #include <SmartMatrix3.h>)
+//#include <SmartLEDShieldV4.h>  // uncomment this line for SmartLED Shield V4 (needs to be before #include <SmartMatrix3.h>)
 #include <SmartMatrix3.h>
-#include "src/colorwheel.c"
-#include "src/gimpbitmap.h"
-#include <IotWebConf.h>
+#include "colorwheel.c"
+#include "gimpbitmap.h"
 
-// -- Initial name of the Thing. Used e.g. as SSID of the own Access Point.
-const char thingName[] = "ClassicMatrix";
-
-// -- Initial password to connect to the Thing, when it creates an own Access Point.
-const char wifiInitialApPassword[] = "classicmatrix";
 #define COLOR_DEPTH 24                  // known working: 24, 48 - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24
 const uint8_t kMatrixWidth = 64;        // known working: 32, 64, 96, 128
 const uint8_t kMatrixHeight = 32;       // known working: 16, 32, 48, 64
 const uint8_t kRefreshDepth = 36;       // known working: 24, 36, 48
 const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save memory, more to keep from dropping frames and automatically lowering refresh rate
-const uint8_t kPanelType = SMARTMATRIX_HUB75_64ROW_MOD32SCAN; //SMARTMATRIX_HUB75_32ROW_MOD16SCAN; // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels, or use SMARTMATRIX_HUB75_64ROW_MOD32SCAN for common 64x64 panels
+const uint8_t kPanelType = SMARTMATRIX_HUB75_32ROW_MOD16SCAN;   // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels
 const uint8_t kMatrixOptions = (SMARTMATRIX_OPTIONS_NONE);      // see http://docs.pixelmatix.com/SmartMatrix for options
 const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
 const uint8_t kScrollingLayerOptions = (SM_SCROLLING_OPTIONS_NONE);
@@ -30,10 +24,13 @@ SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeig
 SMARTMATRIX_ALLOCATE_SCROLLING_LAYER(scrollingLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kScrollingLayerOptions);
 SMARTMATRIX_ALLOCATE_INDEXED_LAYER(indexedLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kIndexedLayerOptions);
 
-const int defaultBrightness = (100*255)/100;    // full (100%) brightness
-//const int defaultBrightness = (15*255)/100;    // dim: 15% brightness
+const int defaultBrightness = (100*255)/100;        // full (100%) brightness
+//const int defaultBrightness = (15*255)/100;       // dim: 15% brightness
 const int defaultScrollOffset = 6;
 const rgb24 defaultBackgroundColor = {0x40, 0, 0};
+
+// Teensy 3.0 has the LED on pin 13
+const int ledPin = 13;
 
 void drawBitmap(int16_t x, int16_t y, const gimp32x32bitmap* bitmap) {
   for(unsigned int i=0; i < bitmap->height; i++) {
@@ -47,48 +44,12 @@ void drawBitmap(int16_t x, int16_t y, const gimp32x32bitmap* bitmap) {
   }
 }
 
-DNSServer dnsServer;
-WebServer server(80);
-
-IotWebConf iotWebConf(thingName, &dnsServer, &server, wifiInitialApPassword);
-
-
-/**
- * Handle web requests to "/" path.
- */
-void handleRoot()
-{
-  // -- Let IotWebConf test and handle captive portal requests.
-  if (iotWebConf.handleCaptivePortal())
-  {
-    // -- Captive portal request were already served.
-    return;
-  }
-  String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
-  s += "<title>IotWebConf 01 Minimal</title></head><body>Hello world!";
-  s += "Go to <a href='config'>configure page</a> to change settings.";
-  s += "</body></html>\n";
-
-  server.send(200, "text/html", s);
-}
-
-
 // the setup() method runs once, when the sketch starts
 void setup() {
+  // initialize the digital pin as an output.
+  pinMode(ledPin, OUTPUT);
+
   Serial.begin(115200);
-  Serial.println();
-  Serial.println("Starting up...");
-
-  // -- Initializing the configuration.
-  iotWebConf.init();
-
-  // -- Set up required URL handlers on the web server.
-  server.on("/", handleRoot);
-  server.on("/config", []{ iotWebConf.handleConfig(); });
-  server.onNotFound([](){ iotWebConf.handleNotFound(); });
-
-  Serial.println("Ready.");
-
 
   matrix.addLayer(&backgroundLayer); 
   matrix.addLayer(&scrollingLayer); 
@@ -129,16 +90,9 @@ void setup() {
 #define DEMO_REFRESH_RATE       1
 #define DEMO_READ_PIXEL         1
 
-
-void loop() {
-  // -- doLoop should be called as frequently as possible.
-  iotWebConf.doLoop();
-}
-
 // the loop() method runs over and over again,
 // as long as the board has power
-void old_loop() {
-
+void loop() {
     int i, j;
     unsigned long currentMillis;
 
@@ -1417,6 +1371,10 @@ void old_loop() {
         scrollingLayer.setFont(font6x10);
         scrollingLayer.setOffsetFromTop(0);
         scrollingLayer.start("Change Refresh Rate", 1);
+
+#if defined(ESP32)
+        scrollingLayer.start("Changing Refresh Rate is Currently Broken in ESP32", 1);
+#endif
 
         backgroundLayer.fillScreen({0,0,0});
         drawBitmap(0,0,&colorwheel);
